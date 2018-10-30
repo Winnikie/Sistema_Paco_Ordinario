7539b3aff6501fbbca2869a6b812a2b0b4806a7a
Miguel Gomez <miguelgomez_@live.com.mx>
2018-10-30 17:37:50 -0600

    Cree las capitas

--- Proyecto/SoftRedes/SoftRedes/Program.cs
+++ Proyecto/SoftRedes/SoftRedes/Program.cs
@@ -16,7 +16,7 @@ namespace SoftRedes
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Usuarios());
+            Application.Run(new Login());
         }
     }
 }
